3ffc0629072633a18bcb5ac93be6c8975da515b3
paleonluna <paleonluna@gmail.com>
2018-05-14 19:57:04 -0700

    added in wooden walls assets

---
+++ Assets/LevelDesignSet1_WoodenWalls/Scripts/RotateInfinite.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateInfinite : MonoBehaviour {
+
+	public float xAxis, yAxis, zAxis;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (xAxis, yAxis, zAxis);
+	}
+}
