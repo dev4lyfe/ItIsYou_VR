1dd467b001bf5b12d6b48c9969ecaf53b73ec050
paleonluna <paleonluna@gmail.com>
2018-05-15 23:41:04 -0700

    perfected candle nuance logic

--- Assets/Scripts/FlickeringLight.cs
+++ Assets/Scripts/FlickeringLight.cs
@@ -4,9 +4,7 @@ using System.Collections;
 [RequireComponent(typeof(Light))]
 public class FlickeringLight : MonoBehaviour {
 	Light lt;
-	float originalRange;
-
-	[Space(10)]
+	float originalRange;	[Space(10)]
 
 	[Header("Light customization")]
 	[Space(10)]
@@ -27,12 +25,42 @@ public class FlickeringLight : MonoBehaviour {
 	[Tooltip("The maximum range of your light (radius).")]
 	float maxRange = 100f;
 
-	void Start() {
-		lt = GetComponent<Light>();
-	}
 	void FixedUpdate() {
 		lt.intensity = Random.Range(minIntensity, maxIntensity);
 		lt.range =  Random.Range(minRange, maxRange);
 		lt.color = lightColor;
+	}
+
+
+	Vector3 StartingPosition;
+	Vector3 NextPosition;
+	Vector3 LastPosition;
+	float translationTimer;
+	float translationTime = 7f;
+	float lerpVal;
+	bool bIsTranslating;
+
+	float flameOffset = .001f;
+
+	void Start() {
+		lt = GetComponent<Light>();
+		StartingPosition = transform.position;
+	}
+	void Update() {
+		if (bIsTranslating) {
+			lerpVal += Time.deltaTime * translationTime;
+			transform.position = Vector3.Lerp (LastPosition, NextPosition, lerpVal);
+			if (lerpVal > 1) {
+				lerpVal = 0;
+				bIsTranslating = false;
+			}
+		} else {
+			//pick new point
+			Vector3 newVec = new Vector3(Random.Range(-flameOffset,flameOffset),  Random.Range(-flameOffset,flameOffset), Random.Range(-flameOffset,flameOffset));
+			LastPosition = transform.position;
+			NextPosition = StartingPosition + newVec;
+			bIsTranslating = true;
+			translationTime = Random.Range (18, 27);
+		}
 	}
 }
