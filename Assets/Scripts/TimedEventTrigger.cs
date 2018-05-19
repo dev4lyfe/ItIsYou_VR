f7c79b5a8d5545ba47d760836890a2ca82635b78
paleonluna <paleonluna@gmail.com>
2018-05-19 16:42:10 -0700

    add vfx and scripts

---
+++ Assets/Scripts/TimedEventTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedEventTrigger : UnityEventTrigger {
+
+	public float timeUntilTriggered;
+	protected float timer;
+	protected bool isBeingTimed;
+	public bool notTriggeredByCollider;
+	public bool autoStart;
+	// Use this for initialization
+	void Start () {
+		if (autoStart)
+			ForceStart ();
+	}
+
+	protected override void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player" && !hasPlayed && !notTriggeredByCollider) 
+		{
+			isBeingTimed = true;
+		}
+	}
+
+	protected void OnTriggerExit (Collider other) {
+		if (other.tag == "Player" && !hasPlayed && !notTriggeredByCollider) {
+			isBeingTimed = false;
+		}
+	}
+
+	// Update is called once per frame
+	protected void Update () {
+		if (isBeingTimed)
+			timer += Time.deltaTime;
+		if (timer > timeUntilTriggered && !hasPlayed)
+			ExecuteEvent ();
+	}
+
+	public void ForceStart() {
+		isBeingTimed = true;
+	}
+}
---
+++ Assets/Scripts/UnityEventTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventTrigger : MonoBehaviour {
+
+	public UnityEvent myUnityEvent;
+	public bool isInfinitelyTriggerable;
+	protected bool hasPlayed;
+
+	protected virtual void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player" && !hasPlayed) {
+			ExecuteEvent ();
+		}
+	}
+
+	void Start() {
+		if (GetComponent<MeshRenderer>() != null)
+			GetComponent<MeshRenderer> ().enabled = false;
+	}
+
+	public void ExecuteEvent () {
+		if (!isInfinitelyTriggerable)hasPlayed = true;
+		if (myUnityEvent.GetPersistentTarget (0) != null) {
+			myUnityEvent.Invoke ();
+		}
+	}
+}
