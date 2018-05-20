490a0c7b6950816b82c64a5c04a82b425ba90271
zachary rock-zelazny <zacharyrock@hotmail.com>
2018-05-20 00:29:11 -0700

    Falling Photos

---
+++ Assets/Scripts/FallingPaper.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPaper : MonoBehaviour {
+
+    Vector3 endPosition;
+    Quaternion endRotation;
+
+    bool active = false;
+
+    float minFallSpeed = 0.4f;
+    float maxFallSpeed = 0.5f;
+    float fallSpeed;
+
+    float minRotateSpeed = 0.1f;
+    float maxRotateSpeed = 0.2f;
+    float rotateSpeed;
+
+    Vector3 rotationVelocity;
+
+	void Start () {
+        endPosition = transform.position;
+        endRotation = transform.rotation;
+
+        transform.position += Vector3.up * 10f;
+        foreach(Renderer rend in GetComponentsInChildren<Renderer>()) {
+            rend.enabled = false;
+        }
+        Activate();
+	}
+	
+	public void Activate () {
+        foreach (Renderer rend in GetComponentsInChildren<Renderer>())
+        {
+            rend.enabled = true;
+        }
+        active = true;
+        rotationVelocity = Random.onUnitSphere;
+        rotateSpeed = Random.Range(minRotateSpeed, maxRotateSpeed) * 360f;
+        fallSpeed = Random.Range(minFallSpeed, maxFallSpeed);
+	}
+
+	private void Update()
+	{
+        if(active) {
+            if(Vector3.Distance(transform.position, endPosition) < 0.1f) {
+                active = false;
+            } else if(Vector3.Distance(transform.position, endPosition) < 0.11f) {
+                transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+
+
+                //transform.rotation = Quaternion.Lerp(transform.rotation, endRotation, Time.deltaTime*3f);
+                GetComponent<Rigidbody>().useGravity = true;
+                //transform.Rotate(
+                //    rotationVelocity.x * rotateSpeed * Time.deltaTime,
+                //    rotationVelocity.y * rotateSpeed * Time.deltaTime,
+                //    rotationVelocity.z * rotateSpeed * Time.deltaTime
+                //);
+            } else {
+				transform.position += Vector3.down * fallSpeed * Time.deltaTime;
+				transform.Rotate(
+					rotationVelocity.x * rotateSpeed * Time.deltaTime, 
+					rotationVelocity.y * rotateSpeed * Time.deltaTime, 
+					rotationVelocity.z * rotateSpeed * Time.deltaTime
+				);
+            }
+        }
+	}
+
+	//private void OnDrawGizmos()
+	//{
+ //       Gizmos.color = Color.red;
+ //       Gizmos.DrawWireSphere(transform.position + Vector3.up * 10f, 0.5f);
+	//}
+}
---
+++ Assets/Scripts/FallingPictureManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPictureManager : MonoBehaviour {
+
+    public GameObject picturePrefab;
+
+    //public float songLength = 100f;
+
+    public float minPhotoSpawn = 0.1f;
+    public float maxPhotoSpawn = 0.2f;
+
+    float currentPhotoSpawnTime;
+    float spawnTimer = 0f;
+
+    public Texture[] photoTextures;
+    List<Texture> availablePhotoTextures;
+
+	void Start () {
+        availablePhotoTextures = new List<Texture>(photoTextures);
+        currentPhotoSpawnTime = Random.Range(minPhotoSpawn, maxPhotoSpawn);
+	}
+	
+	void Update () {
+        if (availablePhotoTextures.Count > 0)
+        {
+            spawnTimer += Time.deltaTime;
+            if (spawnTimer > currentPhotoSpawnTime)
+            {
+                currentPhotoSpawnTime = Random.Range(minPhotoSpawn, maxPhotoSpawn);
+                spawnTimer = 0f;
+                SpawnPhoto();
+            }
+        } else {
+            availablePhotoTextures = new List<Texture>(photoTextures);
+        }
+	}
+
+    void SpawnPhoto() {
+        Vector3 spawnPos = transform.position + new Vector3(Random.Range(-5.5f, 5.5f), 0f, Random.Range(-6f, 6f));
+        int index = Random.Range(0, availablePhotoTextures.Count);
+        Texture texture = availablePhotoTextures[index];
+        availablePhotoTextures.RemoveAt(index);
+
+        GameObject photo = Instantiate(picturePrefab, spawnPos, picturePrefab.transform.rotation);
+        transform.Rotate(0f, 0f, Random.Range(0f, 360f));
+        photo.transform.Find("Photo").GetComponent<Renderer>().material.mainTexture = texture;
+    }
+}
