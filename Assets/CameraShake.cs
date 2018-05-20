0d73579599295010fecf3fe884093c8da0df0457
paleonluna <paleonluna@gmail.com>
2018-05-20 14:32:56 -0700

    pushing up camera shake

---
+++ Assets/CameraShake.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+	public Transform camTransform;
+
+	public float shakeDuration;
+
+	public float shakeAmount = 0.7f;
+
+	public float decreaseFactor = 1.0f;
+
+	Vector3 originalPos;
+
+	void Awake()
+	{
+		if (camTransform == null) {
+			camTransform = GetComponent<Transform> ();
+		}
+	}
+
+	public void Shake()
+	{
+		shakeDuration = .1f;
+	}
+
+	void OnEnable(){
+		originalPos = camTransform.localPosition;
+	}
+
+	void Update()
+	{
+		if (shakeDuration > 0) {
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		} else {
+			shakeDuration = 0f;
+			camTransform.localPosition = originalPos;
+		}
+	}
+}
