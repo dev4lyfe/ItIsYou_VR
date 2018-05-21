186c58292ca6bc95538a5c5b7d5394d60d1edf78
paleonluna <paleonluna@gmail.com>
2018-05-20 19:09:08 -0700

    added in great falling paper logic

---
+++ Assets/DebugTimeScaler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTimeScaler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	bool changedTimeScale = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Q)) {
+			Time.timeScale = 2f;
+			changedTimeScale = true;
+		} else if (Input.GetKey (KeyCode.W)) {
+			Time.timeScale = 3f;
+			changedTimeScale = true;
+		} else if (Input.GetKey (KeyCode.E)) {
+			Time.timeScale = 10f;
+			changedTimeScale = true;
+		} else if(changedTimeScale) {
+			changedTimeScale = false;
+			Time.timeScale = 1f;
+		}
+	}
+}
