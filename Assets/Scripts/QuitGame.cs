9cb716ae082c77a4b0315717e722546f6bd598a5
Pablo Leon-Luna <paleonluna@gmail.com>
2018-05-20 20:31:34 -0700

    Main Menu UI stuff.

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+
+	public void QuitApplication(){
+		Application.Quit();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
