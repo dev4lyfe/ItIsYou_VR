c66073f0f3d280370879276e8821a7dfeb2cec24
Poolmans <matthewsauer09@gmail.com>
2018-05-20 18:16:06 -0700

    BS

---
+++ Assets/Scripts/VRChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRChecker : MonoBehaviour {
+
+	public GameObject mouseController;
+
+	// Use this for initialization
+	void Start () {
+
+		if(!XRDevice.isPresent){
+
+			mouseController.SetActive(true);
+
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
