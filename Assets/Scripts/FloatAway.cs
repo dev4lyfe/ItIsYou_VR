d4b3448829a78c2e59740a36f1f7514a9100d9ce
Poolmans <matthewsauer09@gmail.com>
2018-05-20 20:29:38 -0700

    Float away script and prefab

--- Assets/Scripts/FloatAway.cs
+++ Assets/Scripts/FloatAway.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class FloatAway : MonoBehaviour {
 
-	
+	public Vector3 floatDir;
+	public float floatSpeed = 0.1f;
+	public bool isActive = false;
+
 
 	// Use this for initialization
 	void Start () {
 			
+			floatDir = new Vector3(Random.Range(-1f,1f), 1.0f, Random.Range(-1f,1f));
+			Debug.DrawRay(transform.position, floatDir);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if(isActive){
+
+			Float();
+		}
+
 	}
 
-	public void floatAway(){
+	void Float(){
 
+		transform.Translate(floatDir * floatSpeed, Space.World);
+		transform.Rotate(Random.Range(0,100)*Time.deltaTime,Random.Range(0,100)*Time.deltaTime,Random.Range(0,100)*Time.deltaTime, Space.World);
 
+	}
 
+	public void StartFloat(){
+		isActive = true;
 	}
 }
